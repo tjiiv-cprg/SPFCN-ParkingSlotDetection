fde82cbc64cea40d672af2d5d143f46041413f4f
Gao Zhong <2587291794@qq.com>
2020-09-15 10:39:41 +0800

    Speed up SPFCN Light to ~40FPS in CPU

--- SPFCN_Light/slot_detector.py
+++ SPFCN_Light/slot_detector.py
@@ -4,11 +4,11 @@
 
     Example:
         model_parameter_path = "..."
-        detector = Detector(model_parameter_path)
+        detector = Detector(model_parameter_path, device_id)
         inference_result = detector(inference_image)
 
     Data form:
-        inference_image: Gray Image, torch.Tensor with Size([224, 224])
+        inference_image: Gray Image, numpy.array with Size([224, 224])
         inference_result: list of slot_points
 
     Requirements:
@@ -16,7 +16,7 @@
         numpy == 1.16.5
         opencv == 3.4.2
 
-    2019.11.28
+    2020.09.15
 """
 
 from cv2 import line
@@ -68,106 +68,98 @@ class Hourglass(nn.Module):
 
 
 class Detector(object):
-    def __init__(self, file_name):
+    def __init__(self, file_name, device_id=0):
         super().__init__()
-        self._network = Hourglass([1, 40, 56, 55, 60, 59, 61, 59, 60, 55, 56, 40, 3]).cuda()
+        self.device = torch.device('cpu' if device_id < 0 else 'cuda:%d' % device_id)
+        self._network = Hourglass([1, 40, 56, 55, 60, 59, 61, 59, 60, 55, 56, 40, 3]).to(self.device)
         self._network.load_state_dict(torch.load(file_name), strict=True)
         print("Success load file {}.".format(file_name))
         self._network.eval()
 
-    @staticmethod
-    def _mask_detection(output, threshold=0):
-        temp_h = torch.ones((1, 224)).cuda()
-        temp_w = torch.ones((224, 1)).cuda()
+        self.temp_h = torch.ones((1, 224)).to(self.device)
+        self.temp_w = torch.ones((224, 1)).to(self.device)
 
-        left = torch.cat((output[1:, :], temp_h), dim=0)
-        right = torch.cat((temp_h, output[:-1, :]), dim=0)
-        up = torch.cat((output[:, 1:], temp_w), dim=1)
-        down = torch.cat((temp_w, output[:, :-1]), dim=1)
+    @torch.no_grad()
+    def __call__(self, inference_image):
+        inference_image = torch.from_numpy(inference_image).to(self.device).float()
+        inference_image = ((inference_image - torch.mean(inference_image)) / torch.std(inference_image))
 
-        mask = (output > threshold) * (output > left) * (output > right) * (output > up) * (output > down)
-        return mask.float(), torch.where(mask), mask.int().sum().item()
+        outputs = self._network(inference_image.unsqueeze_(dim=0).unsqueeze_(dim=0))[0]
 
-    def __call__(self, inference_image, threshold=0.1):
-        inference_image = ((inference_image - torch.mean(inference_image))
-                           / torch.std(inference_image)).unsqueeze_(dim=0).unsqueeze_(dim=0)
-        with torch.no_grad():
-            outputs = self._network(inference_image)
-            mask, mask_points, mask_point_count = self._mask_detection(outputs[0, 0])
-            entry = outputs[0, 1].sigmoid_()
-            side = outputs[0, 2].sigmoid_()
+        output = outputs[0]
+        left = torch.cat((output[1:, :], self.temp_h), dim=0)
+        right = torch.cat((self.temp_h, output[:-1, :]), dim=0)
+        up = torch.cat((output[:, 1:], self.temp_w), dim=1)
+        down = torch.cat((self.temp_w, output[:, :-1]), dim=1)
+        mask = (output > 0) * (output > left) * (output > right) * (output > up) * (output > down)
+        mask, mask_points, mask_point_count = mask.float(), torch.where(mask), mask.int().sum().item()
 
-            result = []
-            for mask_point_index1 in range(mask_point_count):
-                x1 = mask_points[0][mask_point_index1].item()
-                y1 = mask_points[1][mask_point_index1].item()
-                if x1 < 16 or 208 < x1 or y1 < 16 or 208 < y1:
+        entry = outputs[1].sigmoid_()
+        side = outputs[2].sigmoid_()
+
+        result = []
+        for mask_point_index1 in range(mask_point_count - 1):
+            x1 = mask_points[0][mask_point_index1].item()
+            y1 = mask_points[1][mask_point_index1].item()
+            if x1 < 16 or 208 < x1 or y1 < 16 or 208 < y1:
+                continue
+
+            for mask_point_index2 in range(mask_point_index1 + 1, mask_point_count):
+                x2 = mask_points[0][mask_point_index2].item()
+                y2 = mask_points[1][mask_point_index2].item()
+                if x2 < 16 or 208 < x2 or y2 < 16 or 208 < y2:
                     continue
 
-                for mask_point_index2 in range(mask_point_index1 + 1, mask_point_count):
-                    x2 = mask_points[0][mask_point_index2].item()
-                    y2 = mask_points[1][mask_point_index2].item()
+                distance = np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+                if distance < 56 or 72 < distance < 128 or 144 < distance:
+                    continue
 
-                    if x2 < 16 or 208 < x2 or y2 < 16 or 208 < y2:
-                        continue
+                if y1 < y2:
+                    y_min = y1 - 1
+                    y_max = y2 + 2
+                    canvas = np.zeros([x2 - x1 + 3, y2 - y1 + 3])
+                    line(canvas, (1, 1), (y2 - y1 + 1, x2 - x1 + 1), 1, thickness=1)
+                else:
+                    y_min = y2 - 1
+                    y_max = y1 + 2
+                    canvas = np.zeros([x2 - x1 + 3, y1 - y2 + 3])
+                    line(canvas, (1, x2 - x1 + 1), (y1 - y2 + 1, 1), 1, thickness=1)
+                canvas = torch.from_numpy(canvas).float().to(self.device)
+                if (canvas * mask[x1 - 1:x2 + 2, y_min:y_max]).sum() != 2:
+                    continue
 
-                    distance = np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-                    # print("distance", distance)
-                    if distance < 56 or 72 < distance < 128 or 144 < distance:
-                        continue
+                score_entry = (canvas * entry[x1 - 1:x2 + 2, y_min:y_max]).sum() / distance
+                if score_entry < 0.25:
+                    continue
 
-                    if y1 < y2:
-                        y_min = y1 - 1
-                        y_max = y2 + 2
-                        canvas = np.zeros([x2 - x1 + 3, y2 - y1 + 3])
-                        line(canvas, (1, 1), (y2 - y1 + 1, x2 - x1 + 1), 1, 2)
-                    else:
-                        y_min = y2 - 1
-                        y_max = y1 + 2
-                        canvas = np.zeros([x2 - x1 + 3, y1 - y2 + 3])
-                        line(canvas, (1, x2 - x1 + 1), (y1 - y2 + 1, 1), 1, 2)
-                    canvas = torch.from_numpy(canvas).float().cuda()
-                    if canvas.shape != torch.Size([x2 - x1 + 3, y_max - y_min]):
-                        continue
+                direct = (side[x1 - 2:x1 + 3, y1 - 2:y1]).sum() > (side[x1 - 2:x1 + 3, y1:y1 + 3]).sum()
+                if direct:
+                    direct_vector_y = 16 * (x1 - x2) / distance
+                    direct_vector_x = -16 * (y1 - y2) / distance
+                else:
+                    direct_vector_y = -16 * (x1 - x2) / distance
+                    direct_vector_x = 16 * (y1 - y2) / distance
 
-                    if (canvas * mask[x1 - 1:x2 + 2, y_min:y_max]).sum() != 2:
-                        continue
+                vec_x = int(direct_vector_x)
+                vec_y = int(direct_vector_y)
+                side_score1 = side[x1 + vec_x - 2:x1 + vec_x + 3, y1 + vec_y - 2:y1 + vec_y + 3].sum()
+                if side_score1 < 0.25:
+                    continue
 
-                    score_entry = (canvas * entry[x1 - 1:x2 + 2, y_min:y_max]).sum() / distance
-                    # print("score_entry", score_entry.item())
-                    if score_entry < threshold:
-                        continue
+                side_score2 = side[x2 + vec_x - 2:x2 + vec_x + 3, y2 + vec_y - 2:y2 + vec_y + 3].sum()
+                if side_score2 < 0.25:
+                    continue
 
-                    direct = (side[x1 - 2:x1 + 3, y1 - 2:y1]).sum() > (side[x1 - 2:x1 + 3, y1:y1 + 3]).sum()
-                    if direct:
-                        direct_vector = (16 * (x1 - x2) / distance, 16 * (y2 - y1) / distance)
-                    else:
-                        direct_vector = (16 * (x2 - x1) / distance, 16 * (y1 - y2) / distance)
+                if score_entry * (side_score1 + side_score2) < 1:
+                    continue
 
-                    vec_x = int(direct_vector[1])
-                    vec_y = int(direct_vector[0])
-                    side_score1 = side[x1 + vec_x - 2:x1 + vec_x + 3, y1 + vec_y - 2:y1 + vec_y + 3].sum()
-                    side_score2 = side[x2 + vec_x - 2:x2 + vec_x + 3, y2 + vec_y - 2:y2 + vec_y + 3].sum()
-                    # print("side score", side_score1, side_score2)
-                    if side_score1 < threshold or side_score2 < threshold:
-                        continue
+                pt0 = (y1, x1)
+                pt1 = (y2, x2)
+                pt2 = (y1 + direct_vector_y * 6, x1 + direct_vector_x * 6)
+                pt3 = (y2 + direct_vector_y * 6, x2 + direct_vector_x * 6)
 
-                    # print("score_final", score_entry * (side_score1 + side_score2))
-                    if score_entry * (side_score1 + side_score2) < threshold * 8:
-                        continue
-
-                    pt0 = (y1, x1)
-                    pt1 = (y2, x2)
-
-                    if distance < 85:
-                        pt2 = (pt0[0] + direct_vector[0] * 6, pt0[1] + direct_vector[1] * 6)
-                        pt3 = (pt1[0] + direct_vector[0] * 6, pt1[1] + direct_vector[1] * 6)
-                    else:
-                        pt2 = (pt0[0] + direct_vector[0] * 3, pt0[1] + direct_vector[1] * 3)
-                        pt3 = (pt1[0] + direct_vector[0] * 3, pt1[1] + direct_vector[1] * 3)
-
-                    if direct:
-                        result.append((pt0, pt1, pt2, pt3))
-                    else:
-                        result.append((pt1, pt0, pt3, pt2))
+                if direct:
+                    result.append((pt0, pt1, pt2, pt3))
+                else:
+                    result.append((pt1, pt0, pt3, pt2))
         return result
